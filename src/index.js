458e6f2866d639b358ddfe24c8b4a04ddde1a4dd
mohdanas86 <anasalam7739@gmail.com>
2025-11-11 16:29:52 +0530

    refactor: remove duplicate dotenv import in index.js
    - Remove duplicate dotenv import
    - Improve import organization and cleanliness

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import dotenv from 'dotenv';
 dotenv.config();
 
 import http from 'http';
 import { app } from './app.js';
-import dotenv from 'dotenv';
 import databaseconnection from './db/index.js';
 import { apiRateLimit } from './middlewares/ratelimit.middleware.js';
 
